0adf6b5b9530101210775469c61f809904c6e39a
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-23 21:21:10 +0800

    add method to find the student with the highest gwa

--- second_program_highest_gwa_finder/highest_gwa_finder.py
+++ second_program_highest_gwa_finder/highest_gwa_finder.py
@@ -4,3 +4,10 @@ class StudentGwaEvaluator:
     # Setting a target file
     def __init__(self, file_path):
         self.file_path = file_path
+
+    # Find and print the top student with the highest GWA
+    def find_highest_gwa(self):
+        highest_gwa = -1.0
+        top_student = ""
+
+     
