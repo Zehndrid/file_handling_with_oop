107622a25d237888b7b18d3471c3a801e957ee73
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-23 10:42:48 +0800

    adjusted the identations and added an except filenotfounderror to print error: input file missing

--- first_program_even_odd_separator/even_odd_seperator.py
+++ first_program_even_odd_separator/even_odd_seperator.py
@@ -6,13 +6,17 @@ class NumberSeparator:
         def separate_and_save(self, even_file, odd_file):
             try:
                 with open(self.input_file, 'r') as file:
-                all_numbers = [int(line.strip()) for line in file if line.strip().lstrip('-').isdigit()]
+                     all_numbers = [int(line.strip()) for line in file if line.strip().lstrip('-').isdigit()]
 
                 with open(even_file, 'w') as file_even, open(odd_file, 'w') as file_odd:
                     for number in all_numbers:
-                    if number % 2 == 0:
-                        file_even.write(f"{number}\n")
+                         if number % 2 == 0:
+                             file_even.write(f"{number}\n")
                     else:
-                        file_odd.write(f"{number}\n")
+                             file_odd.write(f"{number}\n")
                         
-            print("Success! Numbers separated.")
+                print("Success! Numbers separated.")
+
+            except FileNotFoundError:
+           
+                print("Error: Input file missing.")
