fb72fdb4ed054c5791b71c9adca3e37d526f5b19
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-23 11:09:23 +0800

    adjusted again the identation because that might be the error

--- first_program_even_odd_separator/even_odd_separator.py
+++ first_program_even_odd_separator/even_odd_separator.py
@@ -4,7 +4,7 @@ class NumberSeparator:
     def __init__(self, input_file):
         self.input_file = input_file
         # Read numbers and save to separate files
-def separate_and_save(self, even_file, odd_file):
+    def separate_and_save(self, even_file, odd_file):
             try:
                 # Read file and convert lines to integers
                 with open(self.input_file, 'r') as file:
@@ -15,7 +15,7 @@ def separate_and_save(self, even_file, odd_file):
                     for number in all_numbers:
                          if number % 2 == 0:
                              file_even.write(f"{number}\n")
-                    else:
+                         else:
                              file_odd.write(f"{number}\n")
                         
                 print("Success! Numbers separated.")
