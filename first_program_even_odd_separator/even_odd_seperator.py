58a4fcff14a15cff9acde91ea65bb68d03014489
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-23 10:19:03 +0800

    now it will read the file and will convert it to integers

--- first_program_even_odd_separator/even_odd_seperator.py
+++ first_program_even_odd_separator/even_odd_seperator.py
@@ -5,3 +5,5 @@ class NumberSeparator:
 
         def separate_and_save(self, even_file, odd_file):
             try:
+                with open(self.input_file, 'r') as file:
+                all_numbers = [int(line.strip()) for line in file if line.strip().lstrip('-').isdigit()]
