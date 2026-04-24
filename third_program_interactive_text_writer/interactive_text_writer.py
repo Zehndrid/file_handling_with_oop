0dfdae359e21abdacddac1337c378bbf771ca3ca
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-24 15:29:51 +0800

    Implement write_lines method for user input handling

--- third_program_interactive_text_writer/interactive_text_writer.py
+++ third_program_interactive_text_writer/interactive_text_writer.py
@@ -4,3 +4,7 @@ class LifeStoryWriter:
     # seting an output file
     def __init__(self, output_file):
         self.output_file = output_file
+
+    # Get input and write lines
+    def write_lines(self):
+        print("Write your story. Type 'STOP' to finish.\n")
