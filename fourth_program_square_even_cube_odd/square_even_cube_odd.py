6ec8602e2301fa45a162c4c72bc4ccc4a38b3235
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-24 22:26:31 +0800

    calculating the input and firectly saving it to files

--- fourth_program_square_even_cube_odd/square_even_cube_odd.py
+++ fourth_program_square_even_cube_odd/square_even_cube_odd.py
@@ -4,3 +4,6 @@ class NumberProcessor:
     # Set input file
     def __init__(self, input_file):
         self.input_file = input_file
+    # Calculate and save to files
+    def process_and_save(self, double_file, triple_file):
+        try:
