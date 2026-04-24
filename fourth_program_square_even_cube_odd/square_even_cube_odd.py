b2ea9a4817081d314fb363b7103dcfa439c277ff
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-04-24 22:30:01 +0800

    now it will read the integer numbers from the file

--- fourth_program_square_even_cube_odd/square_even_cube_odd.py
+++ fourth_program_square_even_cube_odd/square_even_cube_odd.py
@@ -7,3 +7,6 @@ class NumberProcessor:
     # Calculate and save to files
     def process_and_save(self, double_file, triple_file):
         try:
+            # Read integers from file
+            with open(self.input_file, 'r') as file:
+                all_numbers = [int(line.strip()) for line in file if line.strip().lstrip('-').isdigit()]
